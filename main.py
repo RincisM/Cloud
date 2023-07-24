016398708c0b2fc80378fb1a9273460fc72afb86
RincisM <rincismelvin@gmail.com>
2023-07-24 17:16:07 +0530

    Added fastapi app

---
+++ main.py
@@ -0,0 +1,40 @@
+import pickle
+
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class PredictionInput(BaseModel):
+    ph: float
+    hardness: float
+    solids: float
+    chloramines: float
+    sulfate: float
+    conductivity: float
+    organic_carbon: float
+    trihalomethanes: float
+    turbidity: float
+
+
+@app.post("/")
+async def root(payload: PredictionInput):
+    with open("model.pickle", "rb") as f:
+        model = pickle.load(f)
+        input = np.asarray(
+            (
+                payload.ph,
+                payload.hardness,
+                payload.solids,
+                payload.chloramines,
+                payload.sulfate,
+                payload.conductivity,
+                payload.organic_carbon,
+                payload.trihalomethanes,
+                payload.turbidity,
+            )
+        ).reshape(1, -1)
+        result = model.predict(input)
+        return {"prediction": result[0]}
